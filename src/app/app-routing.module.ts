70fe4fa94c0ab934150a976b6a2e8d6f615a296d
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-11 02:10:33 +0600

    education component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { EducationComponent } from './education/education.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "about", component: EducationComponent},
+  {path : "experiance", component: EducationComponent},
+  {path : "training", component: EducationComponent},
+  {path : "project", component: EducationComponent},
+  {path : "skill", component: EducationComponent},
+  {path : "experiance", component: EducationComponent},
+  {path : "contact", component: EducationComponent},
+  
+  
+  {path : "education", component: EducationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
