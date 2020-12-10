99067b3e13d9545a9050f1bda3592ef3d6f12cac
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-11 03:49:02 +0600

    landing page modeled and router link added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { EducationComponent } from './education/education.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path : "about", component: EducationComponent},
-  {path : "experiance", component: EducationComponent},
+
+  {path : "", component: AboutComponent},
+  {path : "about", redirectTo: "/" ,pathMatch: "full"},
+  // {path : "experiance", component: EducationComponent},
+  {path : "education", component: EducationComponent},
   {path : "training", component: EducationComponent},
   {path : "project", component: EducationComponent},
   {path : "skill", component: EducationComponent},
   {path : "experiance", component: EducationComponent},
-  {path : "contact", component: EducationComponent},
+  {path : "contact", component: ContactComponent}
   
   
-  {path : "education", component: EducationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EducationComponent } from './education/education.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EducationComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
